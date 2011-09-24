e341dfefb4fb6f7b7352eb0504f0a24a060bc531
Justin Dearing <zippy1981@gmail.com>
2011-09-24 11:28:39 -0400

    Added License and proper versioning.

---
+++ GlobalAssembly.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Just A Programmer LLC")]
+[assembly: AssemblyProduct("AppDomainPowershellRunner")]
+[assembly: AssemblyCopyright("Copyright © Just A Programmer LLC 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.9.*")]
