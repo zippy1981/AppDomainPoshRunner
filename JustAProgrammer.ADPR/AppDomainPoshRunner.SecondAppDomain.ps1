f97f0e902466cfd455b4b1451f608f29bebb5074
Justin Dearing <zippy1981@gmail.com>
2011-09-24 11:14:14 -0400

    Improved script.

--- JustAProgrammer.ADPR/AppDomainPoshRunner.SecondAppDomain.ps1
+++ JustAProgrammer.ADPR/AppDomainPoshRunner.SecondAppDomain.ps1
@@ -1,1 +1,20 @@
-﻿"Hello World"
+﻿#This test adapted from: http://www.eggheadcafe.com/microsoft/Powershell/30766269/how-to-load--unload-dll.aspx
+
+$assemblyPath = "$($ENV:TEMP)\$([Guid]::NewGuid()).dll"
+
+$cSharpCodeProvider = New-Object Microsoft.CSharp.CSharpCodeProvider
+
+$parameters = New-Object System.CodeDom.Compiler.CompilerParameters
+$parameters.GenerateInMemory = $true
+$parameters.GenerateExecutable = $false
+$parameters.OutputAssembly = $assemblyPath
+$cSharpCodeProvider.CompileAssemblyFromSource($parameters, @"
+public static class HelloWorld {
+	public static string SayHello() {
+		return "Hello World";
+	}
+}
+"@) > $null;
+
+Add-Type -Path $assemblyPath;
+[HelloWorld]::SayHello();
--- JustAProgrammer.ADPR/AppDomainPoshRunner.Tester.ps1
+++ JustAProgrammer.ADPR/AppDomainPoshRunner.Tester.ps1
@@ -1,7 +1,6 @@
-﻿Add-Type -Path 'JustAProgrammer.ADPR.dll'
-
+﻿Add-Type -AssemblyName 'JustAProgrammer.ADPR, Version=1.0.0.0, Culture=neutral, PublicKeyToken=622b08f923d495ef'
 try {
-	[JustAProgrammer.ADPR.AppDomainPoshRunner]::RunScriptInAppDomain("AppDomainPoshRunner.SecondAppDomain.ps1");
+	[JustAProgrammer.ADPR.AppDomainPoshRunner]::RunScriptInAppDomain("$(Split-Path -Parent $MyInvocation.MyCommand.Path)\AppDomainPoshRunner.SecondAppDomain.ps1");
 }
 catch {
 	Write-Error $_.Exception;
