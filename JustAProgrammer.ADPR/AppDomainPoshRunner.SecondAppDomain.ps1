048b73ac157bfd1aa5a31a378476194fd1687a13
Justin Dearing <zippy1981@gmail.com>
2011-09-25 22:31:16 -0400

    A slightly better test.

--- JustAProgrammer.ADPR/AppDomainPoshRunner.SecondAppDomain.ps1
+++ JustAProgrammer.ADPR/AppDomainPoshRunner.SecondAppDomain.ps1
@@ -1,20 +1,48 @@
 ﻿#This test adapted from: http://www.eggheadcafe.com/microsoft/Powershell/30766269/how-to-load--unload-dll.aspx
 
-$assemblyPath = "$($ENV:TEMP)\$([Guid]::NewGuid()).dll"
+function Make-Dll {
+	$assemblyPath = "$($ENV:TEMP)\$([Guid]::NewGuid()).dll"
 
-$cSharpCodeProvider = New-Object Microsoft.CSharp.CSharpCodeProvider
-
-$parameters = New-Object System.CodeDom.Compiler.CompilerParameters
-$parameters.GenerateInMemory = $true
-$parameters.GenerateExecutable = $false
-$parameters.OutputAssembly = $assemblyPath
-$cSharpCodeProvider.CompileAssemblyFromSource($parameters, @"
-public static class HelloWorld {
-	public static string SayHello() {
-		return "Hello World";
+	$sourceCode = @"
+	public class HelloWorld {
+		public string SayHello() {
+			return "Hello World";
+		}
+		
+		public string SayHello(Person person) {
+			return string.Format("Hello {0}", person.FirstName);
+		}
 	}
-}
-"@) > $null;
 
-Add-Type -Path $assemblyPath;
-[HelloWorld]::SayHello();
+	public sealed class Person {
+		private string _firstName;
+		public string FirstName { 
+			get { return _firstName; }
+			set { _firstName = value; }
+		}
+		
+		private string _lastName;
+		public string LastName { 
+			get { return _lastName; }
+			set { _lastName = value; }
+		}
+	}
+"@
+
+	Add-Type -TypeDefinition $sourceCode #-OutputAssembly $assemblyPath
+};
+
+Make-Dll;
+
+$person = New-Object Person -Property @{
+	"FirstName" = "Justin";
+	"LastName" = "Dearing";
+}
+
+Make-Dll;
+
+$hw = New-Object HelloWorld
+$Host
+
+Write-Host $hw.SayHello($person);
+
--- JustAProgrammer.ADPR/AppDomainPoshRunner.Tester.ps1
+++ JustAProgrammer.ADPR/AppDomainPoshRunner.Tester.ps1
@@ -1,6 +1,10 @@
-﻿Add-Type -AssemblyName 'JustAProgrammer.ADPR, Version=1.0.0.0, Culture=neutral, PublicKeyToken=622b08f923d495ef'
+﻿Add-Type -AssemblyName 'JustAProgrammer.ADPR, Version=0.9.4285.38564, Culture=neutral, PublicKeyToken=622b08f923d495ef'
 try {
-	[JustAProgrammer.ADPR.AppDomainPoshRunner]::RunScriptInAppDomain("$(Split-Path -Parent $MyInvocation.MyCommand.Path)\AppDomainPoshRunner.SecondAppDomain.ps1");
+    $script = "$(Split-Path -Parent $MyInvocation.MyCommand.Path)\AppDomainPoshRunner.SecondAppDomain.ps1"
+	#[JustAProgrammer.ADPR.AppDomainPoshRunner]::RunScriptInNewAppDomain($script);
+	[JustAProgrammer.ADPR.AppDomainPoshRunner]::RunScriptInPSHost($script, $Host) > $null;
+	#Start-Job -FilePath $script
+    #Invoke-Command -FilePath $script
 }
 catch {
 	Write-Error $_.Exception;
