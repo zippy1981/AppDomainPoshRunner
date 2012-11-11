3684555e5e1d572cf22d12c6183be90db0eb3c24
Justin Dearing <zippy1981@gmail.com>
2012-11-11 01:36:32 -0500

    Taking initial steps towards making Write-Host and friends work.

---
+++ JustAProgrammer.ADPR/ADPRRawUserInterface.cs
@@ -0,0 +1,182 @@
+using System;
+using System.Management.Automation.Host;
+
+namespace JustAProgrammer.ADPR
+{
+    /// <summary>
+    /// A sample implementation of the PSHostRawUserInterface for console
+    /// applications. Members of this class that easily map to the .NET 
+    /// console class are implemented. More complex methods are not 
+    /// implemented and throw a NotImplementedException exception.
+    /// </summary>
+    /// <remarks>Taken from the msdn page <seealso cref="http://msdn.microsoft.com/en-us/library/windows/desktop/ee706570(v=vs.85).aspx">Writing a Windows PowerShell Host Application</seealso></remarks>
+    internal class ADPRRawUserInterface : PSHostRawUserInterface
+    {
+        /// <summary>
+        /// Gets or sets the background color of the displayed text.
+        /// This maps to the corresponding Console.Background property.
+        /// </summary>
+        public override ConsoleColor BackgroundColor
+        {
+            get { return Console.BackgroundColor; }
+            set { Console.BackgroundColor = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the size of the host buffer. In this example the 
+        /// buffer size is adapted from the Console buffer size members.
+        /// </summary>
+        public override Size BufferSize
+        {
+            get { return new Size(Console.BufferWidth, Console.BufferHeight); }
+            set { Console.SetBufferSize(value.Width, value.Height); }
+        }
+
+        /// <summary>
+        /// Gets or sets the size of the displayed cursor. In this example 
+        /// the cursor size is taken directly from the Console.CursorSize 
+        /// property.
+        /// </summary>
+        public override int CursorSize
+        {
+            get { return Console.CursorSize; }
+            set { Console.CursorSize = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the foreground color of the displayed text.
+        /// This maps to the corresponding Console.ForgroundColor property.
+        /// </summary>
+        public override ConsoleColor ForegroundColor
+        {
+            get { return Console.ForegroundColor; }
+            set { Console.ForegroundColor = value; }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the user has pressed a key. This maps   
+        /// to the corresponding Console.KeyAvailable property.
+        /// </summary>
+        public override bool KeyAvailable
+        {
+            get { return Console.KeyAvailable; }
+        }
+
+        /// <summary>
+        /// Gets the dimensions of the largest window that could be 
+        /// rendered in the current display, if the buffer was at the least 
+        /// that large. This example uses the Console.LargestWindowWidth and 
+        /// Console.LargestWindowHeight properties to determine the returned 
+        /// value of this property.
+        /// </summary>
+        public override Size MaxPhysicalWindowSize
+        {
+            get { return new Size(Console.LargestWindowWidth, Console.LargestWindowHeight); }
+        }
+
+        /// <summary>
+        /// Gets the dimentions of the largest window size that can be 
+        /// displayed. This example uses the Console.LargestWindowWidth and 
+        /// console.LargestWindowHeight properties to determine the returned 
+        /// value of this property.
+        /// </summary>
+        public override Size MaxWindowSize
+        {
+            get { return new Size(Console.LargestWindowWidth, Console.LargestWindowHeight); }
+        }
+
+        /// <summary>
+        /// Gets or sets the position of the displayed window. This example 
+        /// uses the Console window position APIs to determine the returned 
+        /// value of this property.
+        /// </summary>
+        public override Coordinates WindowPosition
+        {
+            get { return new Coordinates(Console.WindowLeft, Console.WindowTop); }
+            set { Console.SetWindowPosition(value.X, value.Y); }
+        }
+
+        /// <summary>
+        /// Gets or sets the size of the displayed window. This example 
+        /// uses the corresponding Console window size APIs to determine the  
+        /// returned value of this property.
+        /// </summary>
+        public override Size WindowSize
+        {
+            get { return new Size(Console.WindowWidth, Console.WindowHeight); }
+            set { Console.SetWindowSize(value.Width, value.Height); }
+        }
+
+        /// <summary>
+        /// Gets or sets the title of the displayed window. The example 
+        /// maps the Console.Title property to the value of this property.
+        /// </summary>
+        public override string WindowTitle
+        {
+            get { return Console.Title; }
+            set { Console.Title = value; }
+        }
+
+        /// <summary>
+        /// This API resets the input buffer. In this example this 
+        /// functionality is not needed so the method returns nothing.
+        /// </summary>
+        public override void FlushInputBuffer()
+        {
+            Console.In.ReadToEnd();
+        }
+
+        /// <summary>
+        /// This API returns a rectangular region of the screen buffer. In 
+        /// this example this functionality is not needed so the method throws 
+        /// a NotImplementException exception.
+        /// </summary>
+        /// <param name="rectangle">Defines the size of the rectangle.</param>
+        /// <returns>Throws a NotImplementedException exception.</returns>
+        public override BufferCell[,] GetBufferContents(Rectangle rectangle)
+        {
+            throw new NotImplementedException("The method or operation is not implemented.");
+        }
+
+        /// <summary>
+        /// This API reads a pressed, released, or pressed and released keystroke 
+        /// from the keyboard device, blocking processing until a keystroke is 
+        /// typed that matches the specified keystroke options.
+        /// </summary>
+        /// <param name="options">Options, such as IncludeKeyDown,  used when 
+        /// reading the keyboard.</param>
+        /// <returns>Throws a NotImplementedException exception.</returns>
+        public override KeyInfo ReadKey(ReadKeyOptions options)
+        {
+            var keyInfo = Console.ReadKey((options & ReadKeyOptions.NoEcho) == ReadKeyOptions.NoEcho);
+            throw new NotImplementedException("TODO: Handle this");
+        }
+
+        public override Coordinates CursorPosition
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override void ScrollBufferContents(Rectangle source, Coordinates destination, Rectangle clip, BufferCell fill)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetBufferContents(Rectangle rectangle, BufferCell fill)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetBufferContents(Coordinates origin, BufferCell[,] contents)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ JustAProgrammer.ADPR/ADPRUserInterface.cs
@@ -0,0 +1,253 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Management.Automation;
+using System.Management.Automation.Host;
+
+namespace JustAProgrammer.ADPR
+{
+    /// <summary>
+    /// A sample implementation of the PSHostUserInterface abstract class for 
+    /// console applications. Not all members are implemented. Those that are 
+    /// not implemented throw a NotImplementedException exception. Members that 
+    /// are implemented include those that map easily to Console APIs. 
+    /// </summary>
+    /// <remarks>Taken from the msdn page <seealso cref="http://msdn.microsoft.com/en-us/library/windows/desktop/ee706570(v=vs.85).aspx">Writing a Windows PowerShell Host Application</seealso></remarks>
+    internal class ADPRUserInterface : PSHostUserInterface
+    {
+        /// <summary>
+        /// An instance of the PSRawUserInterface class.
+        /// </summary>
+        private PSHostRawUserInterface myRawUi = new ADPRRawUserInterface();
+
+        /// <summary>
+        /// Gets an instance of the PSRawUserInterface class for this host
+        /// application.
+        /// </summary>
+        public override PSHostRawUserInterface RawUI
+        {
+            get { return this.myRawUi; }
+        }
+
+        /// <summary>
+        /// Prompts the user for input. In this example this functionality is not 
+        /// needed so the method throws a NotImplementException exception.
+        /// </summary>
+        /// <param name="caption">The caption or title of the prompt.</param>
+        /// <param name="message">The text of the prompt.</param>
+        /// <param name="descriptions">A collection of FieldDescription objects that 
+        /// describe each field of the prompt.</param>
+        /// <returns>Throws a NotImplementedException exception.</returns>
+        public override Dictionary<string, PSObject> Prompt(
+                                                            string caption,
+                                                            string message,
+                                                            System.Collections.ObjectModel.Collection<FieldDescription> descriptions)
+        {
+            throw new NotImplementedException(
+                "The method or operation is not implemented.");
+        }
+
+        /// <summary>
+        /// Provides a set of choices that enable the user to choose a 
+        /// single option from a set of options. In this example this 
+        /// functionality is not needed so the method throws a 
+        /// NotImplementException exception.
+        /// </summary>
+        /// <param name="caption">Text that proceeds (a title) the choices.</param>
+        /// <param name="message">A message that describes the choice.</param>
+        /// <param name="choices">A collection of ChoiceDescription objects that describes 
+        /// each choice.</param>
+        /// <param name="defaultChoice">The index of the label in the Choices parameter 
+        /// collection. To indicate no default choice, set to -1.</param>
+        /// <returns>Throws a NotImplementedException exception.</returns>
+        public override int PromptForChoice(string caption, string message, System.Collections.ObjectModel.Collection<ChoiceDescription> choices, int defaultChoice)
+        {
+            throw new NotImplementedException("The method or operation is not implemented.");
+        }
+
+        /// <summary>
+        /// Prompts the user for credentials with a specified prompt window caption, 
+        /// prompt message, user name, and target name. In this example this 
+        /// functionality is not needed so the method throws a 
+        /// NotImplementException exception.
+        /// </summary>
+        /// <param name="caption">The caption for the message window.</param>
+        /// <param name="message">The text of the message.</param>
+        /// <param name="userName">The user name whose credential is to be prompted for.</param>
+        /// <param name="targetName">The name of the target for which the credential is collected.</param>
+        /// <returns>Throws a NotImplementedException exception.</returns>
+        public override PSCredential PromptForCredential(
+                                                         string caption,
+                                                         string message,
+                                                         string userName,
+                                                         string targetName)
+        {
+            throw new NotImplementedException("The method or operation is not implemented.");
+        }
+
+        /// <summary>
+        /// Prompts the user for credentials by using a specified prompt window caption, 
+        /// prompt message, user name and target name, credential types allowed to be 
+        /// returned, and UI behavior options. In this example this functionality 
+        /// is not needed so the method throws a NotImplementException exception.
+        /// </summary>
+        /// <param name="caption">The caption for the message window.</param>
+        /// <param name="message">The text of the message.</param>
+        /// <param name="userName">The user name whose credential is to be prompted for.</param>
+        /// <param name="targetName">The name of the target for which the credential is collected.</param>
+        /// <param name="allowedCredentialTypes">A PSCredentialTypes constant that 
+        /// identifies the type of credentials that can be returned.</param>
+        /// <param name="options">A PSCredentialUIOptions constant that identifies the UI 
+        /// behavior when it gathers the credentials.</param>
+        /// <returns>Throws a NotImplementedException exception.</returns>
+        public override PSCredential PromptForCredential(
+                                                         string caption,
+                                                         string message,
+                                                         string userName,
+                                                         string targetName,
+                                                         PSCredentialTypes allowedCredentialTypes,
+                                                         PSCredentialUIOptions options)
+        {
+            throw new NotImplementedException("The method or operation is not implemented.");
+        }
+
+        /// <summary>
+        /// Reads characters that are entered by the user until a newline 
+        /// (carriage return) is encountered.
+        /// </summary>
+        /// <returns>The characters that are entered by the user.</returns>
+        public override string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Reads characters entered by the user until a newline (carriage return) 
+        /// is encountered and returns the characters as a secure string. In this 
+        /// example this functionality is not needed so the method throws a 
+        /// NotImplementException exception.
+        /// </summary>
+        /// <returns>Throws a NotImplemented exception.</returns>
+        public override System.Security.SecureString ReadLineAsSecureString()
+        {
+            throw new NotImplementedException("The method or operation is not implemented.");
+        }
+
+        /// <summary>
+        /// Writes characters to the output display of the host.
+        /// </summary>
+        /// <param name="value">The characters to be written.</param>
+        public override void Write(string value)
+        {
+            Console.Write(value);
+        }
+
+        /// <summary>
+        /// Writes characters to the output display of the host and specifies the 
+        /// foreground and background colors of the characters. This implementation 
+        /// ignores the colors.
+        /// </summary>
+        /// <param name="foregroundColor">The color of the characters.</param>
+        /// <param name="backgroundColor">The backgound color to use.</param>
+        /// <param name="value">The characters to be written.</param>
+        public override void Write(
+                                   ConsoleColor foregroundColor,
+                                   ConsoleColor backgroundColor,
+                                   string value)
+        {
+            var oldColors = new[] {Console.ForegroundColor, Console.BackgroundColor};
+            Console.ForegroundColor = foregroundColor;
+            Console.BackgroundColor = backgroundColor;
+            Console.Write(value);
+            Console.ForegroundColor = oldColors[0];
+            Console.BackgroundColor = oldColors[1];
+        }
+
+        /// <summary>
+        /// Writes a debug message to the output display of the host.
+        /// </summary>
+        /// <param name="message">The debug message that is displayed.</param>
+        public override void WriteDebugLine(string message)
+        {
+            Console.WriteLine(String.Format(
+                                            CultureInfo.CurrentCulture,
+                                            "DEBUG: {0}",
+                                            message));
+        }
+
+        /// <summary>
+        /// Writes an error message to the output display of the host.
+        /// </summary>
+        /// <param name="value">The error message that is displayed.</param>
+        public override void WriteErrorLine(string value)
+        {
+            Console.WriteLine(String.Format(
+                                            CultureInfo.CurrentCulture,
+                                            "ERROR: {0}",
+                                            value));
+        }
+
+        /// <summary>
+        /// Writes a newline character (carriage return) 
+        /// to the output display of the host. 
+        /// </summary>
+        public override void WriteLine()
+        {
+            System.Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Writes a line of characters to the output display of the host 
+        /// and appends a newline character(carriage return). 
+        /// </summary>
+        /// <param name="value">The line to be written.</param>
+        public override void WriteLine(string value)
+        {
+            Console.WriteLine(value);
+        }
+
+        /// <summary>
+        /// Writes a line of characters to the output display of the host 
+        /// with foreground and background colors and appends a newline (carriage return). 
+        /// </summary>
+        /// <param name="foregroundColor">The forground color of the display. </param>
+        /// <param name="backgroundColor">The background color of the display. </param>
+        /// <param name="value">The line to be written.</param>
+        public override void WriteLine(ConsoleColor foregroundColor, ConsoleColor backgroundColor, string value)
+        {
+            var oldColors = new[] { Console.ForegroundColor, Console.BackgroundColor };
+            Console.ForegroundColor = foregroundColor;
+            Console.BackgroundColor = backgroundColor;
+            Console.WriteLine(value);
+            Console.ForegroundColor = oldColors[0];
+            Console.BackgroundColor = oldColors[1];
+        }
+
+        /// <summary>
+        /// Writes a progress report to the output display of the host.
+        /// </summary>
+        /// <param name="sourceId">Unique identifier of the source of the record. </param>
+        /// <param name="record">A ProgressReport object.</param>
+        public override void WriteProgress(long sourceId, ProgressRecord record)
+        {
+        }
+
+        /// <summary>
+        /// Writes a verbose message to the output display of the host.
+        /// </summary>
+        /// <param name="message">The verbose message that is displayed.</param>
+        public override void WriteVerboseLine(string message)
+        {
+            Console.WriteLine(String.Format(CultureInfo.CurrentCulture, "VERBOSE: {0}", message));
+        }
+
+        /// <summary>
+        /// Writes a warning message to the output display of the host.
+        /// </summary>
+        /// <param name="message">The warning message that is displayed.</param>
+        public override void WriteWarningLine(string message)
+        {
+            Console.WriteLine(String.Format(CultureInfo.CurrentCulture, "WARNING: {0}", message));
+        }
+    }
+}
